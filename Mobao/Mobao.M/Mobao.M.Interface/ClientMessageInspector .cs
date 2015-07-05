1d4812cec0735828ea4af5da63ef0cfbc3c45ac2
chenkai <ck0074451665@outlook.com>
2015-07-05 19:47:17 +0800

    ck

---
+++ Mobao/Mobao.M/Mobao.M.Interface/ClientMessageInspector .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobao.M.WinForm
+{
+    public class ClientMessageInspector:System.ServiceModel.Dispatcher.IClientMessageInspector 
+    {
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, System.ServiceModel.IClientChannel channel)
+        {
+            return null;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mobao/Mobao.M/Mobao.M.Interface/EndpointBehavior .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobao.M.WinForm
+{
+    public class EndpointBehavior : System.ServiceModel.Description.IEndpointBehavior
+    {
+        public void AddBindingParameters(System.ServiceModel.Description.ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyClientBehavior(System.ServiceModel.Description.ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.ClientRuntime clientRuntime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyDispatchBehavior(System.ServiceModel.Description.ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.EndpointDispatcher endpointDispatcher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate(System.ServiceModel.Description.ServiceEndpoint endpoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
